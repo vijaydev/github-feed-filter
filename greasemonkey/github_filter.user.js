a16b777bd8e11d97b6cc0ace9ac4ea6e9bee7ec0
Weslly Honorato <weslly.honorato@gmail.com>
2013-09-02 00:52:36 -0300

    Updated to support the latest Github layout

--- greasemonkey/github_filter.user.js
+++ greasemonkey/github_filter.user.js
@@ -5,30 +5,47 @@
 // @include        https://github.com/
 // @include        https://github.com/#
 // @include        https://github.com/dashboard
+// @grant          none
 // ==/UserScript==
 
 /* Bugs & TODOS
  * T: Optimize re-runs (next version)
  */
 
+$.expr[':'].contains_ci = function(a, i, m) { return $(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0; };
 var Ghf = {
-  $: unsafeWindow.jQuery, log: unsafeWindow.console.info,
-  feeds: {}, counts: {}, ui: {},
-
+  feeds: {}, counts: {}, ui: {}, timer_id: '',
   init: function() {
     this.searchterm = '';
     this.rel = '';
+    this.selected_item = '';
     this.feeds = {};
+    this.oldcount = this.counts['all'] || 0;
     this.counts = { 'all': 0, 'commits': 0, 'comments': 0, 'issues': 0 };
-    this.ui = { body: [], bottom_bar: "<div class='bottom-bar'> </div> </div>", top_bar: "<div class='repos' id='your_feeds'> <div class='top-bar'> <h2 class='count'>News Feeds <em></em></h2> </div><div class='filter-bar'> <input class='filter_input' placeholder='Find a repository feed…' type='search'><ul class='repo_filterer'> <li class='all_repos'><a href='#' class='repo_filter filter_selected' rel='all'>All Feeds</a></li> <li><a href='#' class='repo_filter' rel='commits'>Commits</a></li> <li><a href='#' class='repo_filter' rel='comments'>Comments</a></li> <li><a href='#' class='repo_filter' rel='issues'>Issues</a></li> </ul> </div>" };
+    this.ui = { body: [], bottom: "</div> </div></div>", top: "<div class='repos box box-small' id='your_feeds'> <div class='box-header'> <h3 class='box-title'>News Feed <span class='box-title-count'></span></h3> </div><div class='box-body'><div class='filter-repos filter-bar'> <input class='filter-input js-filterable-field' placeholder='Find a repository feed…' type='text'><ul class='repo-filterer'> <li class='all_repos'><a href='javascript:;' class='repo-filter js-repo-filter-tab filter-selected' rel='all'>All Feeds</a></li> <li><a href='javascript:;' class='js-repo-filter-tab repo-filter' rel='commits'>Commits</a></li> <li><a href='javascript:;' class='js-repo-filter-tab repo-filter' rel='comments'>Comments</a></li> <li><a href='javascript:;' class='js-repo-filter-tab repo-filter' rel='issues'>Issues</a></li> </ul></div>" };
   },
   run: function() {
     this.init();
-    this.searchterm = Ghf.$("#your_feeds .filter_input").val();
-    this.rel = Ghf.$('#your_feeds .repo_filterer a.filter_selected').attr('rel');
+    this.searchterm = $("#your_feeds .filter-input").val();
+    this.rel = $('#your_feeds .repo-filterer a.filter-selected').attr('rel');
+    this.selected_item = $('#feed_listing li[selected="1"] span[class!="spancount"]').text();
+    if(!this.read())
+      this.timer_id = setInterval(this.read(), 1000);
+  },
+  read: function() {
     this.read_feeds();
-    this.create_panel();
-    this.show_panel();
+    if(this.oldcount < this.counts['all']) {
+      if(this.timer_id !== undefined)
+        clearInterval(this.timer_id);
+      this.oldcount = this.counts['all'];
+      this.act();
+      return true;
+    }
+    return false;
+  },
+  act: function() {
+    this.create_ui();
+    this.show_ui();
     this.script_events();
     this.monitor_page();
   },
@@ -37,13 +54,18 @@ var Ghf = {
       var keys = [];
       for(var k in obj) keys.push(k);
       return keys;
-    },
-    add_style: function(css) {
-      var style = document.createElement('style'); style.type = 'text/css'; style.innerHTML = css;
-      Ghf.$('head').append(style);
     }
   },
   add_feed: function(name, key, idx) {
+    if(key === 'push') key = 'commits';
+    else if(key.indexOf("_comment") != -1) key = 'comments';
+    else if(key.indexOf("issues_") == 0) key = 'issues';
+    else return;
+
+    if(name.indexOf("@") != -1)
+      name = name.split("@")[0];
+    if(name.indexOf("#") != -1)
+      name = name.split("#")[0];
     var f = this.feeds[name];
     if(f === undefined) {
       f = { all: [idx] };
@@ -63,76 +85,100 @@ var Ghf = {
   },
   read_feeds: function() {
     var self = this;
-    Ghf.$('div.alert').each(function(idx) {
-      var title = Ghf.$(this).find('div.title');
-      var cat = Ghf.$(this).attr("class").split(' ')[1], x = -1;
-      switch(cat) {
-        case 'issues_opened': case 'issues_reopened': case 'issues_closed':
-          x = 2, cat = 'issues';
-        break;
-        case 'issues_comment': case 'commit_comment':
-          x = cat === 'issues_comment' ? 2 : 1, cat = 'comments';
-        break;
-        case 'push':
-          x = 1, cat = 'commits';
-        break;
-      }
-      if(x != -1)
-        self.add_feed(Ghf.$(title.find('a')[x]).html(), cat, idx);
+    $('div.alert').each(function(idx) {
+      var title = $(this).find('div.title');
+      var cat = $(this).attr('class').split(' ')[1];
+      self.add_feed($(title.find('a')[1]).html(), cat, idx);
     });
   },
-  create_panel: function() {
+  create_ui: function() {
     var self = this;
     var repos = self.Utils.keys(self.feeds);
-    self.ui.body.push('<ul id="feed_listing" class="repo_list">');
-    for each(repo in repos) {
+    self.ui.body.push('<div style="padding: 5px; font-size: 12px; text-align: right;" id="reset_filter"><a>Reset Filter</a></div>');
+    self.ui.body.push('<ul id="feed_listing" class="repo-list js-repo-list">');
+    $.each(repos, function(idx, repo) {
       var r = repo.split('/');
       var cats = self.Utils.keys(self.feeds[repo]);
-      self.ui.body.push('<li class="public ' + cats.join(' ') + '"><a class="feedlink" href="#"><span class="owner">' + r[0] + '</span>/<span class="repo">' + r[1] + '</span></a>');
-      Ghf.$.each(cats, function(idx, val) {
-        self.ui.body.push('<span class="spancount" rel="' + val + '">(' + self.feeds[repo][val].length + ')</span>');
+      self.ui.body.push('<li class="public ' + cats.join(' ') + '"><a class="feedlink repo-list-item css-truncate" data-userrepo="' + r[0] + "/" + r[1] + '" href="#"><span class="repo-icon octicon octicon-repo"></span><span class="owner">' + r[0] + '</span>/<span class="repo">' + r[1] + '</span>');
+      $.each(cats, function(idx, val) {
+        self.ui.body.push('<span class="spancount stars" rel="' + val + '">(' + self.feeds[repo][val].length + ')</span>');
       });
-      self.ui.body.push('</li>');
-    }
+      self.ui.body.push('</a></li>');
+    });
     self.ui.body.push('</ul>');
-
-    this.Utils.add_style('.feedlink { background:none !important; display:inline-block !important; } \n' + '.spancount { padding:0px 2px 5px 0 !important; color:#99A4AA !important; font-size:14px !important; }');
   },
-  show_panel: function() {
-    Ghf.$('div#your_feeds').remove();
-    Ghf.$('div#your_repos').after(this.ui.top_bar + this.ui.body.join('') + this.ui.bottom_bar);
+  show_ui: function() {
+    $('div#your_feeds').remove();
+    $('.dashboard-sidebar').prepend(this.ui.top + this.ui.body.join('') + this.ui.bottom);
   },
   script_events: function() {
+    var self = this;
     this.set_spans('all');
     this.setup_search();
     this.attach_name_handlers();
     this.setup_cat_filters();
+    this.reset_filter_event();
 
-    Ghf.$("#your_feeds .filter_input").val(this.searchterm);
+    $("#your_feeds .filter-input").val(this.searchterm);
     if(this.rel !== '') {
-      Ghf.$('#your_feeds a.repo_filter[rel="' + this.rel + '"]').click();
+      $('#your_feeds a.repo-filter[rel="' + this.rel + '"]').click();
     }
+    if(self.selected_item !== '') {
+      $.each($('#feed_listing li'), function(i, item) {
+        if(self.selected_item == $(item).find('span[class!="spancount"]').text()) {
+          $(this).find('a').click();
+        }
+      });
+    }
+  },
+  reset_filter_event: function() {
+    var self = this;
+    $('div#reset_filter').click(function() {
+      self.highlight_item();
+      self.hide_feeds();
+      var r = $('#your_feeds .filter-selected').attr('rel');
+      $.each($('ul#feed_listing li:visible a'), function(i, item) {
+        $.each(self.feeds[$(item).data('userrepo')][r], self.show_feed_n);
+      });
+    });
   },
   attach_name_handlers: function() {
     var self = this;
-    Ghf.$('#feed_listing li a').click(function(evt) {
+    $('#feed_listing li a').click(function(evt) {
       self.hide_feeds();
-      Ghf.$.each(self.feeds[Ghf.$(this).text()][Ghf.$(this).siblings('span:visible').attr('rel')], self.show_feed_n);
+      self.highlight_item($(this).parent());
+      $.each(self.feeds[$(this).data('userrepo')][$(this).children('span.stars:visible').attr('rel')], self.show_feed_n);
       evt.preventDefault();
       evt.stopPropagation();
     });
   },
+  highlight_item: function(to_hl) {
+    $.each($('#feed_listing li'), function(i, item) {
+      if($(item) !== to_hl) {
+        $(item).css('background-color', '#FFF');
+        $(item).removeAttr('selected');
+      }
+    });
+    if(to_hl !== undefined) {
+      to_hl.css('background-color', '#F2F0B6');
+      to_hl.attr('selected', '1');
+    }
+  },
   setup_cat_filters: function() {
     var self = this;
-    Ghf.$('#your_feeds .repo_filter').click(function(evt) {
-      var t = Ghf.$(this), r = t.attr('rel');
-      t.parents('ul').find('a').removeClass('filter_selected');
-      t.addClass('filter_selected');
+    $('#your_feeds .repo-filter').click(function(evt) {
+      var t = $(this), r = t.attr('rel');
+      t.parents('ul').find('a').removeClass('filter-selected');
+      t.addClass('filter-selected');
       self.set_spans(r);
-      self.search(Ghf.$('#your_feeds .filter_input').val(), r);
+      self.search($('#your_feeds .filter-input').val(), r);
       self.hide_feeds();
-      Ghf.$.each(Ghf.$('ul#feed_listing li:visible a'), function(i, item) {
-        Ghf.$.each(self.feeds[Ghf.$(item).text()][r], self.show_feed_n);
+      if($('ul#feed_listing li[selected="1"]:visible').length > 0)
+        item_selector = 'ul#feed_listing li[selected="1"]:visible a';
+      else
+        item_selector = 'ul#feed_listing li:visible a';
+      $.each($(item_selector), function(i, item) {
+        $.each(self.feeds[$(item).data('userrepo')][r], self.show_feed_n);
       });
       evt.preventDefault();
       evt.stopPropagation();
@@ -140,35 +186,34 @@ var Ghf = {
   },
   setup_search: function() {
     var self = this;
-    Ghf.$('#your_feeds .filter_input').addClass('native').bind('keyup blur click', function(e) { self.search(this.value); });
+    $('#your_feeds .filter-input').addClass('native').bind('keyup blur click', function(e) { self.search(this.value); });
   },
   search: function(srch, rel) {
-    var items = Ghf.$('ul#feed_listing li').hide();
+    var items = $('ul#feed_listing li').hide();
     if(!rel)
-      rel = Ghf.$('#your_feeds .repo_filterer a.filter_selected').attr('rel');
-    Ghf.$('ul#feed_listing').find('li.' + rel).show();
+      rel = $('#your_feeds .repo-filterer a.filter-selected').attr('rel');
+    $('ul#feed_listing').find('li.' + rel).show();
     srch != "" && items.filter(":not(:contains_ci('" + srch + "'))").hide();
   },
   monitor_page: function() {
-    Ghf.$('div.news .pagination a').click(function() {
+    $('div.news .pagination a').click(function() {
       setTimeout(function() { Ghf.run(); }, 2000);
     });
   },
   hide_feeds: function() {
-    Ghf.$('div.alert').hide();
+    $('div.alert').hide();
   },
   show_feed_n: function(i, n) {
-    Ghf.$('div.alert:eq(' + n + ')').show();
+    $('div.alert:eq(' + n + ')').show();
   },
   set_count: function(n) {
-    Ghf.$("#your_feeds h2.count em").html("(" + n + ")");
+    $('#your_feeds h3.box-title span').html('(' + n + ')');
   },
   set_spans: function(rel) {
     this.set_count(this.counts[rel]);
-    var f = Ghf.$('ul#feed_listing');
-    f.find("span[rel]").hide();
+    var f = $('ul#feed_listing');
+    f.find('span[rel]').hide();
     f.find('span[rel="' + rel + '"]').show();
   }
 };
-
 Ghf.run();
